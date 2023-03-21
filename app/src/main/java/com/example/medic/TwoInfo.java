9986590b30a179251cf54a230eeb8f5eb379662f
WSR-2 <iwsr2@mail.ru>
2023-03-21 12:54:26 +0800

    Initial

--- app/src/main/java/com/example/medic/TwoInfo.java
+++ app/src/main/java/com/example/medic/TwoInfo.java
@@ -9,5 +9,6 @@ public class TwoInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.onboard3);
+        //gfg
     }
 }
